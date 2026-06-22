d85b207a4bc2edcef25c8560265cb44b32e617a4
Alext_v <sharman2fire.90@yandex.ru>
2026-06-23 00:09:34 +0300

    feat: handle exceptions

--- src/main/java/com/dev/cloud_file_storage/advice/ExceptionApiHandler.java
+++ src/main/java/com/dev/cloud_file_storage/advice/ExceptionApiHandler.java
@@ -2,8 +2,10 @@ package com.dev.cloud_file_storage.advice;
 
 import com.dev.cloud_file_storage.dto.ErrorResponse;
 import com.dev.cloud_file_storage.exception.UsernameAlreadyExsistException;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,17 +15,35 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class ExceptionApiHandler {
 
-    @ExceptionHandler(UsernameNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleNotFound(UsernameNotFoundException e) {
+    //400 //validate
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadRequest(ConstraintViolationException e) {
         log.error(e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
 
+    //401
+    @ExceptionHandler({UsernameNotFoundException.class, BadCredentialsException.class})
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handleUnauthorized(Exception e) {
+        log.error(e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    //409
     @ExceptionHandler(UsernameAlreadyExsistException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleConflict(UsernameAlreadyExsistException e) {
         log.error(e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
+    }
+
+    //500
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleInternalServerError(Exception e) {
+        log.error(e.getMessage(), e);
+        return new ErrorResponse("Unknown error");
     }
 }
