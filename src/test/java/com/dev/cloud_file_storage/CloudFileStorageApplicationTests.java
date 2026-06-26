03029cf593b77db495a828628906ee4fc89f68b5
Alext_v <sharman2fire.90@yandex.ru>
2026-06-26 16:44:04 +0300

    feat: setup WebTestClient

--- src/test/java/com/dev/cloud_file_storage/CloudFileStorageApplicationTests.java
+++ src/test/java/com/dev/cloud_file_storage/CloudFileStorageApplicationTests.java
@@ -1,15 +1,33 @@
 package com.dev.cloud_file_storage;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudFileStorageApplicationTests {
+
+	@Autowired
+	private WebTestClient webClient;
+
+	@Test
+	@DisplayName("ReturnStatus200")
+	void shouldReturnSuccessfulHealthCheck() {
+
+		webClient.get()
+				.uri("/")
+				.exchange()
+				.expectStatus().is2xxSuccessful();
+//				.expectBody(String.class).isEqualTo("UP");
+	}
 
 	@Test
 	void contextLoads() {
 	}
+
 
 }
--- src/test/java/com/dev/cloud_file_storage/TestcontainersConfiguration.java
+++ src/test/java/com/dev/cloud_file_storage/TestcontainersConfiguration.java
@@ -3,6 +3,9 @@ package com.dev.cloud_file_storage;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
@@ -20,6 +23,13 @@ class TestcontainersConfiguration {
 	@ServiceConnection(name = "redis")
 	GenericContainer<?> redisContainer() {
 		return new GenericContainer<>(DockerImageName.parse("redis:8.6.4-alpine")).withExposedPorts(6379);
+	}
+
+	@Bean
+	public WebTestClient webTestClient(WebApplicationContext context) {
+		return MockMvcWebTestClient
+				.bindToApplicationContext(context)
+				.build();
 	}
 
 }
