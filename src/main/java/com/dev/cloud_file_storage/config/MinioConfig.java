f7bb75b39a65be824bdb4f643bd89f8c3090dd3a
Alext_v <sharman2fire.90@yandex.ru>
2026-07-03 08:43:01 +0300

    feat: add main bucket ("user-files")

--- src/main/java/com/dev/cloud_file_storage/config/MinioConfig.java
+++ src/main/java/com/dev/cloud_file_storage/config/MinioConfig.java
@@ -1,8 +1,14 @@
 package com.dev.cloud_file_storage.config;
 
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import io.minio.errors.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 @Configuration
 public class MinioConfig {
@@ -13,6 +19,13 @@ public class MinioConfig {
                 .endpoint("http://localhost:9000")
                 .credentials("minioadmin", "minioadmin")
                 .build();
+    }
+
+    @Bean
+    public void mainBucket() throws ServerException, InsufficientDataException,
+            ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException,
+            InvalidResponseException, XmlParserException, InternalException {
+        minioClient().makeBucket(MakeBucketArgs.builder().bucket("user-files").build());
     }
 
 //    @Bean
