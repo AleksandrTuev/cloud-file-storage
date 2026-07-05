ded2ede17415909b25d50ae78e0960ad47646448
Alext_v <sharman2fire.90@yandex.ru>
2026-07-05 17:08:21 +0300

    refactor

---
+++ src/main/java/com/dev/cloud_file_storage/utils/ProjectConstants.java
@@ -0,0 +1,8 @@
+package com.dev.cloud_file_storage.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ProjectConstants {
+    public static final String NAME_MAIN_BUCKET = "user-files";
+}
